0b37baa0ebfadb8da416baf42c41569d18f1a191
Morgana Galamba <mbfg2@cin.ufpe.br>
2022-10-10 00:35:46 -0300

    setup payment View

--- Sinve/Sinve/View/PaymentViewController.swift
+++ Sinve/Sinve/View/PaymentViewController.swift
@@ -8,11 +8,53 @@
 import UIKit
 
 class PaymentViewController: UIViewController {
-
+    
+    public let ok: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = UIButton.Configuration.filled()
+        button.setTitle("Realizar outra venda", for: .normal)
+        button.backgroundColor = UIColor.clear
+        button.addTarget(self, action:#selector(okPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    public var confirm: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(named: "PineTree")
+        label.text = "Venda realizada!"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Pagamento"
+        //self.title = "Pagamento"
+        let logo = UIImage(named: "logoName.png")
+        let imageView = UIImageView(image:logo)
+        imageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = imageView
+        
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        
         view.backgroundColor = UIColor(named: "BackGround")
+        view.addSubview(confirm)
+        view.addSubview(ok)
+        setupConstraints()
+        
+    }
+    
+    private func setupConstraints(){
+        confirm.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        confirm.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        ok.topAnchor.constraint(equalTo: confirm.bottomAnchor, constant: 16).isActive = true
+        ok.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    @objc func okPressed() {
+        navigationController?.popToRootViewController(animated: true)
     }
 
 }
