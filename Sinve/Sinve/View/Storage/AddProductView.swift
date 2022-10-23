50403dd951031bb82276e092f0fba6d76f63ac07
Morgana Galamba <mbfg2@cin.ufpe.br>
2022-10-23 11:35:48 -0300

    add ui of add product view

---
+++ Sinve/Sinve/View/Storage/AddProductView.swift
@@ -0,0 +1,195 @@
+//
+//  AddProductView.swift
+//  Sinve
+//
+//  Created by Morgana Galamba on 22/10/22.
+//
+
+import UIKit
+
+class AddProductView: UIView {
+    
+    var delegate: AddProviderProtocol?
+    
+    let mainStack: UIStackView = {
+        let stack = UIStackView()
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        stack.spacing = 16
+        stack.axis = .vertical
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    let code: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder:"00987673822",
+                                        keyboard: .numberPad, title: "Código do produto")
+        input.configure(with: model)
+        return input
+    }()
+    
+    let name: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder: "Skipbo jogo de cartas",
+                                        keyboard: .default, title: "Nome do produto")
+        input.configure(with: model)
+        return input
+    }()
+    
+    //setar p caixa
+    let amount: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder: "25",
+                                        keyboard: .numberPad, title: "Quantidade")
+        input.configure(with: model)
+        return input
+    }()
+    
+    let price: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder: "10",
+                                        keyboard: .numberPad, title: "Preço por unidade ")
+        input.configure(with: model)
+        return input
+    }()
+    
+    let category: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder: "Jogos",
+                                        keyboard: .numberPad, title: "Categoria")
+        input.configure(with: model)
+        return input
+    }()
+       
+    public let addProductButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Cadastrar", for: .normal)
+        button.configuration = UIButton.Configuration.filled()
+        button.backgroundColor = UIColor(named: "BackGround")
+        button.addTarget(nil, action: #selector(addProduct), for: .touchUpInside)
+        return button
+    }()
+    
+    let date: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder: "",
+                                        keyboard: .numbersAndPunctuation, title: "Data de validade")
+        input.configure(with: model)
+        return input
+    }()
+    
+    let provider: TextInputSinve = {
+        let input = TextInputSinve()
+        let model = TextInputSinveModel(placeholder: "Empresa", keyboard: .numberPad, title: "Fornecedor")
+        input.configure(with: model)
+        return input
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setupConstraints()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        self.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        self.endEditing(true)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews(){
+        addSubview(mainStack)
+        addSubview(addProductButton)
+                
+        mainStack.addArrangedSubview(code)
+        mainStack.addArrangedSubview(name)
+        mainStack.addArrangedSubview(amount)
+        mainStack.addArrangedSubview(price)
+        mainStack.addArrangedSubview(category)
+        mainStack.addArrangedSubview(date)
+        mainStack.addArrangedSubview(provider)
+        //mainStack.addArrangedSubview(timeDelivery)
+    }
+    
+    private func setupConstraints(){
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            mainStack.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainStack.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainStack.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -130)
+            //mainStack.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 1)
+        ])
+        
+        addProductButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            addProductButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            addProductButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -64)
+        ])
+    }
+    
+    @objc func addProduct(){
+        
+    }
+    
+    let slash = "/"
+    let digits = CharacterSet(charactersIn: "0123456789")
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        //Deleting
+        if string.count == 0 {
+            var text = textField.text!
+            let start = text.startIndex
+            let beginRange = text.index(start, offsetBy: range.location)
+            let endRange = text.index(start, offsetBy: range.location + range.length)
+            text = String(text.prefix(upTo: beginRange))
+                + String(text.suffix(from: endRange))
+            text = text.replacingOccurrences(of: "/", with: "")
+            if text.count >= 3 {
+                text = String(text.prefix(2)) + "/" + String(text.dropFirst(2))
+            }
+
+            textField.text = text
+
+            return false
+        }
+
+        //Typing
+
+        let count = textField.text!.count
+        guard string.count == 1,
+            count < 6,
+            let scalar = Character(string).unicodeScalars.first else {
+                return false
+        }
+
+        let isDigit = digits.contains(scalar)
+
+        switch count {
+        case 0, 3..<6 :
+            return isDigit
+        case 1:
+            if isDigit {
+                textField.text = textField.text! + string + "/"
+            }
+            return false
+        case 2:
+            if string == slash {
+                return true
+            } else {
+                textField.text = textField.text! + "/" + string
+                return false
+            }
+        default:
+            return false
+        }
+    }
+}
--- Sinve/Sinve/View/Storage/AddProductViewController.swift
+++ Sinve/Sinve/View/Storage/AddProductViewController.swift
@@ -8,12 +8,100 @@
 import UIKit
 
 class AddProductViewController: UIViewController {
+    
+    let addProductView = AddProductView()
+    let pickerView = UIPickerView()
+    let datePickerView = UIDatePicker()
+    let providerPickerView = UIPickerView()
+    let items = ["Caderno", "Jogos","Caneta","Variados"]
+    var fornecedores: [Fornecedor] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Cadastrar Produto"
+        self.view = addProductView
         self.navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = UIColor(named: "BackGround")
+        
+        fillProvider()
+        
+        pickerView.delegate = self
+        pickerView.backgroundColor = .white
+        pickerView.tag = 1
+        addProductView.category.input.inputView = pickerView
+        
+        datePickerView.datePickerMode = .date
+        datePickerView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 200)
+        datePickerView.backgroundColor = .white
+        datePickerView.contentVerticalAlignment = .center
+        datePickerView.contentHorizontalAlignment = .center
+        datePickerView.addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
+        addProductView.date.input.inputView = datePickerView
+        //addProductView.delegate = self
+        
+        providerPickerView.delegate = self
+        providerPickerView.tag = 2
+        providerPickerView.backgroundColor = .white
+        addProductView.provider.input.inputView = providerPickerView
+        
+       
     }
     
+    func fillProvider() {
+        let url = URL(string: "http://ec2-54-89-160-231.compute-1.amazonaws.com:5500/fornecedor")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+
+            if let result = try? JSONDecoder().decode([Fornecedor].self, from: data!) {
+                self.fornecedores = result
+            } else {
+                print("Erro ao decodificar dados da API")
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+}
+
+extension AddProductViewController: UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker){
+
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        addProductView.date.input.text = selectedDate
+        
+        print("Selected value \(selectedDate)")
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            return items.count
+        } else {
+            return fornecedores.count
+        }
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return items[row]
+        } else {
+            return fornecedores[row].nomeFantasia
+        }
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selected = items[row]
+        
+        addProductView.category.input.text = selected
+    }
 }
